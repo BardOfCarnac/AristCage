74f98e13d003ccfbcbbf35c7fbe80811b68fd8d5
BardOfCarnac <114069933+BardOfCarnac@users.noreply.github.com>
2026-07-03 23:26:13 +0100

    Refactor activatePresence function for timing

--- js/presence.js
+++ js/presence.js
@@ -2,10 +2,8 @@ function activatePresence() {
   document.querySelectorAll(".entry").forEach((entry, index) => {
     entry.classList.remove("present");
 
-    requestAnimationFrame(() => {
-      setTimeout(() => {
-        entry.classList.add("present");
-      }, index * 80);
-    });
+    setTimeout(() => {
+      entry.classList.add("present");
+    }, 120 + index * 90);
   });
 }
